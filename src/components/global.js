b2567fe76d221e0d34f2a93807bcf9ebcc5786ac
scottbedard <scottbedard90@gmail.com>
2016-11-27 22:33:46 -0700

    Create a global select component for cross browser normalization

--- src/components/global.js
+++ src/components/global.js
@@ -5,4 +5,5 @@ export default {
     'v-button': require('./ui/button'),
     'v-content': require('./layout/content'),
     'v-page': require('./layout/page'),
+    'v-select': require('./ui/select'),
 };
---
+++ src/components/ui/select.vue
@@ -0,0 +1,43 @@
+<style lang="scss" scoped>@import 'core';
+    div {
+        position: relative;
+    }
+
+    span {
+        $arrow-size: 10px;
+        border-color: #ccc;
+        border-style: solid;
+        border-width: 0 2px 2px 0;
+        display: block;
+        height: $arrow-size;
+        pointer-events: none;
+        position: absolute;
+        right: 12px;
+        top: 50%;
+        transform: translateY(-75%) rotate(45deg);
+        width: $arrow-size;
+    }
+
+    select {
+        appearance: none; // hide the default dropdown in most browsers
+        background-color: transparent;
+        border-radius: 3px;
+        border: 1px solid #ccc;
+        cursor: pointer;
+        padding: 8px;
+        text-indent: 1px;
+        text-overflow: '';
+        width: 100%;
+
+        &::-ms-expand { display: none } // hide the default dropdown arrow in IE
+    }
+</style>
+
+<template>
+    <div>
+        <select>
+            <slot></slot>
+        </select>
+        <span></span>
+    </div>
+</template>
