1e31ccc29855013e21a11d8d2150193b97c8b15f
scottbedard <scottbedard90@gmail.com>
2016-11-28 05:14:42 -0700

    Simplifying the inventory selector

--- src/components/shop/inventory_selector/inventory_selector.vue
+++ src/components/shop/inventory_selector/inventory_selector.vue
@@ -1,75 +1,22 @@
 <style lang="scss" scoped>@import 'core';
-    .option {
-        &:not(:last-of-type) {
-            margin-bottom: 20px;
-        }
-    }
-
-    .values {
-        div {
-            justify-content: space-around;
-            display: flex;
-            flex-wrap: wrap;
-            padding-top: 5px;
-        }
-
-        a {
-            $circle-size: 65px;
-            align-items: center;
-            border-radius: $circle-size / 2;
-            border: 1px solid;
-            display: flex;
-            flex-basis: auto;
-            font-size: 14px;
-            height: $circle-size;
-            justify-content: center;
-            margin: 5px;
-            min-width: $circle-size;
-            text-decoration: none;
-            width: $circle-size;
-            @include transition('background-color, color');
-
-            &:not(.is-clickable) {
-                $not-clickable-color: lighten($off-black, 25%);
-                border-color: $not-clickable-color;
-                color: $not-clickable-color;
-                cursor: default;
-                pointer-events: none;
-            }
-
-            &.is-clickable {
-                border-color: $off-black;
-                color: $off-black;
-
-                &:hover,
-                &.is-selected {
-                    background-color: $off-black;
-                    box-shadow: 0 4px 10px 0 rgba($off-black, 0.45);
-                    color: $off-white;
-                }
-            }
-        }
+    label {
+        display: block;
     }
 </style>
 
 <template>
     <div>
         <div v-for="option in product.options" class="option">
-            <label>{{ option.placeholder }}</label>
-            <div class="values">
-                <div>
-                    <a
-                        href="#"
-                        v-for="value in option.values"
-                        :class="{
-                            'is-clickable': valueIsClickable(value),
-                            'is-selected': valueIsSelected(value),
-                        }"
-                        @click.prevent="onValueClicked(value)">
-                        {{ value.name }}
-                    </a>
-                </div>
-            </div>
+            <label :for="getOptionId(option)">{{ option.name }}</label>
+            <v-select
+                :id="getOptionId(option)"
+                :placeholder="option.placeholder"
+                @change="onValueSelected"
+                @clear="onValueCleared">
+                <option v-for="value in option.values">
+                    {{ value.name }}
+                </option>
+            </v-select>
         </div>
         <pre>{{ selectedValues }}</pre>
     </div>
@@ -108,20 +55,14 @@
             },
         },
         methods: {
-            valueIsSelected(value) {
-                return this.selectedValues.indexOf(value) !== -1;
+            getOptionId(option) {
+                return `option-${ option.id }`;
             },
-            valueIsClickable(value) {
-                // let inventories = this.possibleInventories;
-
-                return true;
+            onValueSelected(value) {
+                console.log ('selected', value);
             },
-            onValueClicked(value) {
-                if (this.valueIsSelected(value)) {
-                    this.selectedValues.splice(this.selectedValues.indexOf(value), 1);
-                } else {
-                    this.selectedValues.push(value);
-                }
+            onValueCleared(value) {
+                console.log ('cleared', value);
             },
         },
         props: [
