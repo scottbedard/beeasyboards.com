bbfc89197fa6e115d953eab5bf2ee33d043d7b1f
Scott Bedard <scottbedard90@gmail.com>
2016-12-05 08:11:51 -0700

    Display thumbnails

--- src/pages/shop/components/product_list/product.vue
+++ src/pages/shop/components/product_list/product.vue
@@ -1,12 +1,42 @@
 <style lang="scss" scoped>@import 'core';
     a {
-        text-align: center;
         padding: 20px;
+        text-align: center;
+
+        .thumbnails {
+            position: relative;
+        }
+
+        img {
+            width: 100%;
+            height: auto;
+            @include transition(opacity, 0.4s);
+        }
+
+        img:nth-of-type(1) {
+            opacity: 1;
+        }
+
+        img:nth-of-type(2) {
+            left: 0;
+            opacity: 0;
+            position: absolute;
+            top: 0;
+        }
+
+        &:hover {
+            img:nth-of-type(1) { opacity: 0 }
+            img:nth-of-type(2) { opacity: 1 }
+        }
     }
 </style>
 
 <template>
     <router-link :to="{ name: 'shop-product', params: { slug: product.slug }}">
+        <div class="thumbnails">
+            <img v-if="firstThumbnail" :src="firstThumbnail.path" :alt="firstThumbnail.title">
+            <img v-if="secondThumbnail" :src="secondThumbnail.path" :alt="secondThumbnail.title">
+        </div>
         <div>{{ product.name }}</div>
         <v-price :base="product.base_price" :current="product.price"></v-price>
     </router-link>
@@ -16,6 +46,22 @@
     export default {
         components: {
             'v-price': require('../price'),
+        },
+        computed: {
+            firstThumbnail() {
+                if (this.product.thumbnails.length < 1) {
+                    return false;
+                }
+
+                return this.product.thumbnails[0];
+            },
+            secondThumbnail() {
+                if (this.product.thumbnails.length < 2) {
+                    return false;
+                }
+
+                return this.product.thumbnails[1];
+            },
         },
         props: [
             'product',
