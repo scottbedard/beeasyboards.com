9ed023a134cb63ebc417030854ef51bb0e6d90ba
Scott Bedard <scottbedard90@gmail.com>
2016-12-12 07:21:02 -0700

    Minor cleanup

--- src/app/filters/money.js
+++ src/app/filters/money.js
@@ -1,12 +1,3 @@
 import Vue from 'vue';
 
-Vue.filter('money', value => {
-
-    console.log ('value', value, typeof value);
-    
-    let amount = '$' + value.toFixed(2);
-
-    return amount.endsWith('.00')
-        ? amount.replace('.00', '')
-        : amount;
-});
+Vue.filter('money', value => '$' + value.toFixed(2).replace('.00', ''));
