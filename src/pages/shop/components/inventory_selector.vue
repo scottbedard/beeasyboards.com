25db1cd813bead32df9a720077126916cf7e4eb4
Scott Bedard <scottbedard90@gmail.com>
2017-01-05 08:20:45 -0700

    Remove old inventory selector

--- src/pages/shop/components/inventory_selector.vue
+++ src/pages/shop/components/inventory_selector.vue
@@ -1,10 +1,12 @@
 <style lang="scss" scoped>@import 'core';
-
+    .option {
+        margin-bottom: 20px;
+    }
 </style>
 
 <template>
-    <div>
-        <div v-for="option in product.options">
+    <form @submit.prevent="onAddToCart" :disabled="formIsDisabled">
+        <div v-for="option in product.options" class="option">
             <v-select
                 v-model="selectedValues.find(selected => selected.option === option.id).value"
                 :placeholder="option.placeholder">
@@ -16,8 +18,11 @@
              --></option>
             </v-select>
         </div>
-        <pre>{{ $data }}</pre>
-    </div>
+        <v-button :disabled="formIsDisabled" type="submit">
+            <v-button-spinner v-if="isLoading"></v-button-spinner>
+            <span>{{ addButtonText }}</span>
+        </v-button>
+    </form>
 </template>
 
 <script>
@@ -28,9 +33,13 @@
         data() {
             return {
                 selectedValues: [],
+                quantity: 1,
             };
         },
         computed: {
+            addButtonText() {
+                return this.isLoading ? 'Adding...' : 'Add to cart';
+            },
             availableInventories() {
                 return this.product.inventories.filter(inventory => inventory.quantity > 0);
             },
@@ -39,13 +48,22 @@
                     return inventory.option_values.map(value => value.id);
                 });
             },
+            formIsDisabled() {
+                return ! Boolean(this.selectedInventory);
+            },
+            selectedInventory() {
+                let selectedValues = this.selectedValues.map(value => Number(value.value));
+
+                return this.product.inventories.find(inventory => {
+                    return inventory.option_values.map(value => value.id).equals(selectedValues);
+                });
+            },
         },
         methods: {
             isAvailable(option, value) {
                 // determine what the selection would be if this value were included
                 let values = this.selectedValues
-                    .filter(value => value.option != option.id)
-                    .filter(value => value.value)
+                    .filter(model => model.value && model.option != option.id)
                     .map(value => Number(value.value));
 
                 values.push(value.id);
@@ -55,6 +73,9 @@
                     return values.intersect(combination).equals(values);
                 }));
             },
+            onAddToCart() {
+                this.$emit('add', this.selectedInventory, this.quantity);
+            },
             setInitialValues() {
                 for (let option of this.product.options) {
                     this.selectedValues.push({ option: option.id, value: null });
@@ -62,6 +83,7 @@
             },
         },
         props: [
+            'isLoading',
             'product',
         ],
     };
